ce03637121e0f4724c9c624e46e55bc45fa747dd
Markus <markus.mork@iths.se>
2022-12-12 16:33:18 +0100

    Init more

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState } from "react"
 import { useSelector, useDispatch } from "react-redux"
-import { increase, decrease } from './store'
+import { increase, decrease } from './store/counterSlice'
 
 const Counter = () => {
     // const [count, setCount] = useState(0)
